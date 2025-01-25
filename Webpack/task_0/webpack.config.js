18f94775f1597fc4bd562b6b942481e524514b72
Pauline <p.mutuku@alustudent.com>
2025-01-25 16:02:51 +0300

    Updates

---
+++ Webpack/task_0/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development", // Change to 'production' for production builds
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+};
